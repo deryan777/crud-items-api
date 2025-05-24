6e6ebdcf9d8a2f9e20ee830b9e3b8d6a22f3fe7d
Jao Ryan <joaoryanoliveira2005@gmail.com>
2025-05-24 12:20:23 -0300

    feat: migrations and , --fix: bugs imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { UsersModule } from './users/users.module';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ItemsModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
